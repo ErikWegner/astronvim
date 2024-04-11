c55c3c055f3f1c473814fccd8009d437e3b1ed6e
Erik Wegner <erik.wegner@regiocom.com>
2024-04-11 13:35:20 +0200

    TabNine

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -9,4 +9,19 @@ return {
       vim.keymap.set("n", "<Leader>U", "<Plug>Nuuid", { desc = "Insert UUID" })
     end,
   },
+  {
+    "codota/tabnine-nvim",
+    build = "./dl_binaries.sh",
+    init = function()
+      require("tabnine").setup {
+        disable_auto_comment = true,
+        accept_keymap = "<C-_>",
+        dismiss_keymap = "<C-[>",
+        debounce_ms = 800,
+        suggestion_color = { gui = "#808080", cterm = 244 },
+        exclude_filetypes = { "TelescopePrompt" },
+        log_file_path = nil, -- absolute path to Tabnine log file
+      }
+    end,
+  },
 }
