61e9b85476bd0440024169cef44b5f713777d35b
Erik Wegner <e_wegner@web.de>
2026-04-29 07:28:30 +0200

    astrolsp

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -30,12 +28,12 @@ return {
       -- control auto formatting on save
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
-        allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
-        },
-        ignore_filetypes = { -- disable format on save for specified filetypes
-          -- "python",
-        },
+        -- allow_filetypes = { -- enable format on save for specified filetypes only
+        --   -- "go",
+        -- },
+        -- ignore_filetypes = { -- disable format on save for specified filetypes
+        --   -- "python",
+        -- },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
@@ -46,10 +44,10 @@ return {
       --   return true
       -- end
     },
-    -- enable servers that you already have installed without mason
-    servers = {
-      -- "pyright"
-    },
+    -- -- enable servers that you already have installed without mason
+    -- servers = {
+    --   -- "pyright"
+    -- },
     -- customize language server configuration passed to `vim.lsp.config`
     -- client specific configuration can also go in `lsp/` in your configuration root (see `:h lsp-config`)
     config = {
@@ -91,29 +89,29 @@ return {
         },
       },
     },
-    -- mappings to be set up on attaching of a language server
-    mappings = {
-      n = {
-        -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
-        gD = {
-          function() vim.lsp.buf.declaration() end,
-          desc = "Declaration of current symbol",
-          cond = "textDocument/declaration",
-        },
-        ["<Leader>uY"] = {
-          function() require("astrolsp.toggles").buffer_semantic_tokens() end,
-          desc = "Toggle LSP semantic highlight (buffer)",
-          cond = function(client)
-            return client:supports_method "textDocument/semanticTokens/full" and vim.lsp.semantic_tokens ~= nil
-          end,
-        },
-      },
-    },
-    -- A custom `on_attach` function to be run after the default `on_attach` function
-    -- takes two parameters `client` and `bufnr`  (`:h lsp-attach`)
-    on_attach = function(client, bufnr)
-      -- this would disable semanticTokensProvider for all clients
-      -- client.server_capabilities.semanticTokensProvider = nil
-    end,
+    -- -- mappings to be set up on attaching of a language server
+    -- mappings = {
+    --   n = {
+    --     -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
+    --     gD = {
+    --       function() vim.lsp.buf.declaration() end,
+    --       desc = "Declaration of current symbol",
+    --       cond = "textDocument/declaration",
+    --     },
+    --     ["<Leader>uY"] = {
+    --       function() require("astrolsp.toggles").buffer_semantic_tokens() end,
+    --       desc = "Toggle LSP semantic highlight (buffer)",
+    --       cond = function(client)
+    --         return client:supports_method "textDocument/semanticTokens/full" and vim.lsp.semantic_tokens ~= nil
+    --       end,
+    --     },
+    --   },
+    -- },
+    -- -- A custom `on_attach` function to be run after the default `on_attach` function
+    -- -- takes two parameters `client` and `bufnr`  (`:h lsp-attach`)
+    -- on_attach = function(client, bufnr)
+    --   -- this would disable semanticTokensProvider for all clients
+    --   -- client.server_capabilities.semanticTokensProvider = nil
+    -- end,
   },
 }
